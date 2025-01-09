d34cb2649b9604a7601e793979af445a7bda33dc
Talha Gergin <talhagergin6@gmail.com>
2025-01-10 01:02:11 +0300

    Initial Commit

---
+++ geminiAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  geminiAI
+//
+//  Created by Talha Gergin on 10.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ geminiAI/geminiAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  geminiAIApp.swift
+//  geminiAI
+//
+//  Created by Talha Gergin on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct geminiAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
